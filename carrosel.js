fdbd3412970bc84c4d7281eb47092ba46097a6da
P#SHAD#L <ppdro19@gmail.com>
2026-02-04 02:21:49 -0300

    Refactor code carousel

--- carrosel.js
+++ carrosel.js
@@ -1,27 +1,32 @@
-const track = document.querySelector(".track")
-const slides = document.querySelectorAll(".slide")
-const nextBtn = document.querySelector(".next")
-const prevBtn = document.querySelector(".prev")
+const track = document.querySelector(".track");
+const slides = Array.from(document.querySelectorAll(".slide"));
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
 
-let index = 0
+const slidesToShow = 3;
+const numPages = Math.ceil(slides.length / slidesToShow);
+let currentPage = 0;
 
 function update() {
-  track.style.transform = `translateX(-${index * 100}%)`
+  const newTransform = -currentPage * 100;
+  track.style.transform = `translateX(${newTransform}%)`;
 }
 
 nextBtn.addEventListener("click", () => {
-  index++
-  if (index >= slides.length) {
-    index = 0
-      }
-      
-  update()
-})
+  currentPage++;
+  if (currentPage >= numPages) {
+    currentPage = 0;
+  }
+  update();
+});
 
 prevBtn.addEventListener("click", () => {
-  index--
-  if (index < 0) {
-    index = slides.length - 1
+  currentPage--;
+  if (currentPage < 0) {
+    currentPage = numPages - 1;
   }
-  update()
-})
+  update();
+});
+
+// Initialize the carousel position
+update();
